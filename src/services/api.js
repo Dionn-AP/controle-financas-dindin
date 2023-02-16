d4e892224fc343c2b284fe1cb6e0bb71814be4c8
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-02-16 01:03:59 -0300

    ''

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
+  // baseURL: 'http://localhost:8000',
   baseURL: 'https://application-dindin.onrender.com',
   timeout: 100000,
   headers: { 'Content-Type': 'application/json' }
