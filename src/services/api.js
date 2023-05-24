8f62bdf82acdd7e62290cef3b8459c814213d49a
Dionn-Pereira <dionnatan@maximize.art.br>
2023-05-24 12:01:58 -0300

    fix: remoção da instancia do sentry

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8000',
-  //baseURL: 'https://application-dindin.onrender.com',
+  //baseURL: 'http://localhost:8000',
+  baseURL: 'https://application-dindin.onrender.com',
   timeout: 100000,
   headers: { 'Content-Type': 'application/json' }
 });
